7640a1928e57b750c6467b81d5000a42542d5f62
Gibloor <gibloor@yandex.ru>
2021-09-06 17:54:07 +0300

    MultLinkChange

--- client/src/components/main/main.tsx
+++ client/src/components/main/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './main.css';
-import video from './videos/onFirstDays.mp4';
+// import video from './videos/onFirstDays.mp4';
 
 const Main = () => {
 
@@ -10,9 +10,14 @@ const Main = () => {
   return (
     <div className="main">
       <p>Multfilm of day: {multName}</p>
-      <video className='main_video' controls>
-        <source src={video} type='video/mp4' />
-      </video>
+      {/* <video className='main_video' controls>
+        <source src={'https://www.youtube.com/embed/Zlmswo0S0e0'} type='video/mp4' />
+      </video> */}
+      <iframe 
+        className='main_video'
+        src="https://www.youtube.com/embed/Zlmswo0S0e0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+      />
       <p>
         number of registered: {registered}
       </p>
